663437e9784fa823baed163c655dae417f4d1bf2
zouzhihao-994 <zouzhihao994@gmail.com>
2020-11-07 00:01:31 +0800

    add RTD

--- src/RTD.go
+++ src/RTD.go
@@ -1,1 +1,27 @@
 package src
+
+import "net"
+
+type RTD struct {
+}
+
+func Bind(host string, port int) (*RTDAddr, error) {
+	if host == "" {
+		return nil, RTDError{msg: "host is blank"}
+	}
+	if port < 0 || port >= 65535 {
+		return nil, RTDError{msg: "port is invalid"}
+	}
+	return &RTDAddr{host: host, port: port}, nil
+}
+
+func Listen(addr *RTDAddr) (*RTDConn, error) {
+	ip := net.ParseIP(addr.host)
+	ua := net.UDPAddr{IP: ip, Port: addr.port, Zone: ""}
+	conn, err := net.ListenUDP("udp", &ua)
+	if err != nil {
+		return nil, err
+	}
+	rc := RTDConn{conn: conn}
+	return &rc, nil
+}
