3fdf8fea8bd21ce06e32845d3ad95feaf604b7cc
zouzhihao-994 <zouzhihao994@gmail.com>
2020-11-07 00:02:58 +0800

    create RTDService

--- src/server/RTDService.go
+++ src/server/RTDService.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/zouzhihao-994/rtd/src"
+)
 
+var host = flag.String("host", "127.0.0.1", "listening host , ip4 or ip6 or localhost")
+var port = flag.Int("port", 9944, "listening port , 5000 ~ 65535")
+
+func main() {
+	// bind
+	ra, err := src.Bind(*host, *port)
+	if err != nil {
+		panic("rtd bind error " + err.Error())
+	}
+	// listen
+	conn, err := src.Listen(ra)
+	if err != nil {
+		panic("rtd listen error " + err.Error())
+	}
+	conn.LoopRead()
 }
